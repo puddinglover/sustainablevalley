e0d30455b6f7ccb234dc0f5ae8c7acca438af242
Daniel Malmros Christiansen <daniel@bymalmros.dk>
2016-12-01 14:00:46 +0100

    pages added

--- app/index.php
+++ app/index.php
@@ -2,6 +2,17 @@
 
 <?php require('nav.php'); ?>
 <main>
-    
+    <section class="landing">
+        <div class="landing__bird">
+            <img src="images/fugl.svg" alt="">
+        </div>
+        <div class="landing__logo">
+            <img src="images/logo.svg" alt="">
+        </div>
+        <a href="#">Explore</a>
+    </section>
+    <section class="">
+
+    </section>
 </main>
 <?php require('footer.php'); ?>
---
+++ app/nav.php
@@ -0,0 +1,9 @@
+<header class="nav-wrapper">
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+        </ul>
+    </nav>
+</header>
