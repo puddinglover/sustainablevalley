831abe700c625d322ae5872eeee0f923e60b1926
Jacob Thesbjerg <jacobthesbjerg@gmail.com>
2016-12-14 20:38:27 +0100

    Mobile nav - menu items styled

--- app/nav.php
+++ app/nav.php
@@ -19,11 +19,28 @@
 
 
   <section class="menu menu--off">
-                <li class="one active"><a href="index.php">Home</a></li>
-                <li class="two"><a href="#">Festival</a></li>
-                <li class="three"><a href="ideavault.php">Idea vault</a></li>
-                <li class="four"><a href="contact.php">Contact</a></li>
-                <li class="five"><a href="tickets.php">Tickets</a></li>
+      <div class="menu__social">
+        <ul class="social-icons">
+            <li>
+                <a href="#"><i class="fa fa-facebook-square fa-3x"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-instagram fa-3x"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-snapchat-square fa-3x"></i></a>
+            </li>
+        </ul>
+    </li>
+  </ul>
+      </div>
+      <div class="menu__container">
+      <div class="one"><a href="index.php">Home</a></div>
+      <div class="two"><a href="#">Festival</a></div>
+      <div class="three"><a href="ideavault.php">Idea vault</a></div>
+      <div class="four"><a href="contact.php">Contact</a></div>
+      <div class="five"><a href="tickets.php">Tickets</a></div>
+    </div>
   </section>
     </nav>
 </header>
