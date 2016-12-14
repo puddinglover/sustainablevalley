f57075ecbb87397ef1bbec5ffe0efc0a3bec6b8c
Jacob Alm <jacob.alm@live.dk>
2016-12-14 17:01:41 +0100

    Added header image, changed styling and added images for tickets.

--- app/tickets.php
+++ app/tickets.php
@@ -8,5 +8,40 @@
         <p>This is a short description of what the page contains and what the user can read/See here!</p>
     </div>
 </section>
+<div class="bgPicture"> </div>
+ <section class="tickets" id="main-ticket">
 
+        <div class="wrapper">
+            <div class="tickets__card">
+                <h2>Partoutticket</h2>
+                <img class="ticket__picture"src="images/partoutticket.jpg" alt="">
+                <article class="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </article>
+                <div class="button button--cards">
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+            <div class="tickets__card">
+                <h2>One day ticket</h2>
+                <img class="ticket__picture"src="images/1dayticket.jpg" alt="">
+                <article class="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </article>
+                <div class="button button--cards">
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+            <div class="tickets__card">
+                <h2>Support ticket</h2>
+                <img  class="ticket__picture"src="images/supportticket.jpg"  alt="">
+                <article class="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </article>
+                <div class="button button--cards">
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php require('footer.php'); ?>
